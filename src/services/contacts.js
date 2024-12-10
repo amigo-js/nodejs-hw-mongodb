521220574237f6cfc7168bea3d13d99cea79ac6d
vito <working.space.js@gmail.com>
2024-12-10 22:23:44 +0100

    Add solution for HW hw3-crud

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,21 @@
-import { Contact } from '../models/contact.js';
+import { Contact } from "../models/contact.js";
 
-export const getAllContacts = async () => {
+export const getAllContactsService = async () => {
   return await Contact.find();
 };
 
-export const getContactById = async (contactID) => {
-  return await Contact.findById(contactID);
+export const getContactByIdService = async (id) => {
+  return await Contact.findById(id);
+};
+
+export const createContactService = async (data) => {
+  return await Contact.create(data);
+};
+
+export const updateContactService = async (id, data) => {
+  return await Contact.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deleteContactService = async (id) => {
+  return await Contact.findByIdAndDelete(id);
 };
